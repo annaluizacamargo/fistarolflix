61246b167bdb10244b3942d23cb7aa1b5017beeb
Anna Luiza Fistarol <anna.fistarol@watch.tv.br>
2024-03-22 16:23:58 -0300

    feat: edit movie entity, and create controller and dto

---
+++ fistarolFlix/src/controllers/MovieController.ts
@@ -0,0 +1,132 @@
+import { Request, Response } from "express";
+import { Movie } from "../entity/Movie";
+import MovieRepository from "../repository/MovieRepository";
+
+/**
+ * Class that represents the movie controller
+ * @class
+ * @method create - Method to create a movie
+ * @method get - Method to get all movies
+ * @method getByMovieCode - Method to get a movie by movie code
+ * @method update - Method to update a movie
+ * @method patch - Method to enable or disable a movie
+ */
+class MovieController {
+  async create(request: Request, response: Response) {
+    const {
+      movieCode,
+      title,
+      genres,
+      duration,
+      year,
+      trailerUrl,
+      sinonpsis,
+      posterUrl,
+      ageRating,
+    } = request.body;
+    const movie = new Movie(
+      movieCode,
+      title,
+      genres,
+      duration,
+      year,
+      trailerUrl,
+      sinonpsis,
+      posterUrl,
+      ageRating,
+      true
+    );
+    const movieRepository = new MovieRepository();
+    const movieAlreadyCreate = await movieRepository.findById(movieCode);
+
+    if (movieAlreadyCreate) {
+      return response.sendStatus(409);
+    }
+
+    const data = await movieRepository.createAndSave(movie);
+
+    if (!data) {
+      return response.sendStatus(400);
+    }
+
+    return response.status(201).json(data);
+  }
+
+  async get(request: Request, response: Response) {
+    const movieRepository = new MovieRepository();
+    const data = await movieRepository.findMovies();
+
+    return response.json(data);
+  }
+
+  async getByMovieCode(request: Request, response: Response) {
+    const { code } = request.params;
+    const movieCodeNumber = parseInt(code);
+
+    if (isNaN(movieCodeNumber)) {
+      return response.sendStatus(400); 
+    }
+
+    const movieRepository = new MovieRepository();
+    const data = await movieRepository.findById(movieCodeNumber);
+
+    if (!data) {
+      return response.sendStatus(404);
+    }
+
+    return response.json(data);
+  }
+
+  async update(request: Request, response: Response) {
+    const {
+      id,
+      movieCode,
+      title,
+      genres,
+      duration,
+      year,
+      trailerUrl,
+      sinonpsis,
+      posterUrl,
+      ageRating,
+    } = request.body;
+    const idNumber = parseInt(id);
+    const movieRepository = new MovieRepository();
+
+    if (isNaN(idNumber)) {
+      return response.sendStatus(400);
+    }
+
+    await movieRepository.updateMovie({
+      id,
+      movieCode,
+      title,
+      genres,
+      duration,
+      year,
+      trailerUrl,
+      sinonpsis,
+      posterUrl,
+      ageRating,
+    });
+
+    return response.sendStatus(200);
+  }
+
+  async patch(request: Request, response: Response) {
+    const { id } = request.params;
+    const { isActive } = request.body;
+    const idNumber = parseInt(id);
+    const movieRepository = new MovieRepository();
+
+    if (isNaN(idNumber)) {
+      return response.sendStatus(400);
+    }
+
+    await movieRepository.enableOrDisableMovie(idNumber, isActive);
+
+    return response.sendStatus(200);
+  }
+}
+
+export default MovieController;
--- fistarolFlix/src/entity/Movie.ts
+++ fistarolFlix/src/entity/Movie.ts
@@ -1,55 +1,77 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IMovie } from "../dto/IMovie";
 
+/**
+ * Class that represents the movie entity
+ * @class
+ * @property {number} id - Movie id (primary key)
+ * @property {number} movieCode - Movie code
+ * @property {string} title - Movie title
+ * @property {string} genres - Movie genres
+ * @property {number} duration - Movie duration
+ * @property {number} year - Movie year
+ * @property {string} trailerUrl - Movie trailer url
+ * @property {string} sinonpsis - Movie sinonpsis
+ * @property {string} posterUrl - Movie poster url
+ * @property {string} ageRating - Movie age rating
+ */
 @Entity()
 export class Movie {
+  constructor(
+    movieCode: IMovie["movieCode"],
+    title: IMovie["title"],
+    genres: IMovie["genres"],
+    duration: IMovie["duration"],
+    year: IMovie["year"],
+    trailerUrl: IMovie["trailerUrl"],
+    sinonpsis: IMovie["sinonpsis"],
+    posterUrl: IMovie["posterUrl"],
+    ageRating: IMovie["ageRating"],
+    isActive: IMovie["isActive"]
+  ) {
+    this.movieCode = movieCode;
+    this.title = title;
+    this.genres = genres;
+    this.duration = duration;
+    this.year = year;
+    this.trailerUrl = trailerUrl;
+    this.sinonpsis = sinonpsis;
+    this.posterUrl = posterUrl;
+    this.ageRating = ageRating;
+    this.isActive = isActive;
+  }
 
-    @PrimaryGeneratedColumn('increment')
-    id: number ;
+  @PrimaryGeneratedColumn("increment")
+  id: number;
 
-    @Column({
-        nullable: true
-    })
-    title: string;
+  @Column()
+  movieCode: number;
 
-    //TODO: change to id genre
-    @Column({
-        nullable: true
-    })
-    genres: string;
+  @Column()
+  title: string;
 
-    @Column({
-        nullable: true
-    })
-    duration: number;
+  //TODO: change to id genre
+  @Column()
+  genres: string;
 
-    @Column({
-        nullable: true
-    })
-    year: number;
+  @Column()
+  duration: number;
 
-    @Column({
-        nullable: true
-    })
-    releaseDate: Date;
+  @Column()
+  year: number;
 
-    @Column({
-        nullable: true
-    })
-    trailerUrl: string;
+  @Column()
+  trailerUrl: string;
 
-    @Column({
-        nullable: true
-    })
-    sinonpsis: string;
+  @Column()
+  sinonpsis: string;
 
-    @Column({
-        nullable: true
-    })
-    posterUrl: string;
+  @Column()
+  posterUrl: string;
 
-    @Column({
-        nullable: true
-    })
-    ageRating: string;
+  @Column()
+  ageRating: string;
 
+  @Column({ default: true })
+  isActive: boolean;
 }
