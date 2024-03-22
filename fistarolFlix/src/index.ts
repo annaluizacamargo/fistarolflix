ce66e243a2bcf91697af214622704f688babce40
Anna Luiza Fistarol <anna.fistarol@watch.tv.br>
2024-03-22 16:25:11 -0300

    feat: edit index

--- fistarolFlix/src/index.ts
+++ fistarolFlix/src/index.ts
@@ -1,75 +1,61 @@
-import express = require("express")
-import { Movie } from "./entity/Movie"
-import { User } from "./entity/User"
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Router } from "express"
-import UserController from "./controllers/UserController"
+import express = require("express");
+import UserController from "./controllers/UserController";
+import MovieController from "./controllers/MovieController";
+import "reflect-metadata";
+require('dotenv').config();
 
+const port = 3000;
 
+const app = express();
 
-const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "123456",
-    database: "postgres",
-    logging: false,
-    synchronize: true,
-    name: "default", //nome da conexão, pode ser qualquer coisa
-    entities: [User, Movie],
-})
+app.use(express.json());
 
-    const port = 3000
-    const app = express();
-AppDataSource.initialize().then(async () => {
+// /** TODO: Implementar
+//  * Middleware for token verification
+//  * @param req
+//  * @param res
+//  * @param next
+//  * @returns
+//  */
+// const verifyToken = (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction
+// ) => {
+//   const token = req.headers.authorization;
 
-    // console.log("Inserting a new user into the database...")
-    // const user = new User()
-    // user.name = "Jane Doe"
-    // user.email = "jane@email.com"
-    // user.password = 'skdnfdjsn'
-    // await AppDataSource.manager.save(user)
-    // console.log("Saved a new user with id: " + user.id)
+//   if (!token) {
+//     return res.status(403).json({ error: "Token não fornecido" });
+//   }
 
-    // //console.log("Loading users from the database...")
-    // // const users = await AppDataSource.manager.find(User)
-    // // console.log("Loaded users: ", users)
+//   verify(token.split(" ")[1], process.env.SECRET_KEY || "", (err, decoded) => {
+//     console.log('env', process.env.SECRET_KEY)
+//     console.log('decoded', decoded)
+//     console.log('err', err)
+//     if (err) {
+//       return res.status(401).json({ error: "Token inválido" });
+//     }
+//     next();
+//   });
+// };
 
-    // const movie = new Movie()
-    // movie.title = "Jane Doe"
-    // movie.ageRating = "a13"
-    // await AppDataSource.manager.save(movie)
-    // console.log("Saved a new movie with id: " + movie.id)
+const userController = new UserController();
+const movieController = new MovieController();
 
-    // console.log("Loading movies from the database...")
-    // const movies = await AppDataSource.manager.find(Movie)
-    // console.log("Loaded movies: ", movies)
+app.post("/login", userController.login);
+app.post("/register", userController.create);
+app.get("/users", userController.get);
+app.get("/user/:id", userController.getById); //TODO: Implementar
+app.get("/user-email/:email", userController.getByEmail);
+app.put("/edit-user/:id", userController.update);
+app.patch("/active-user/:id", userController.patch);
 
-    // console.log("Here you can setup and run express / fastify / any other framework.")
+app.post("/movie-create", movieController.create);
+app.get("/movies", movieController.get);
+app.get("/movie/:code", movieController.getByMovieCode);
+app.put("/edit-movie/:id", movieController.update);
+app.patch("/active-movie/:id", movieController.patch);
 
-    //gere um servidor express
-
-    app.use(express.json()) //o método use é usado para adicionar middlewares
-
-    app.get('/', (req: any, res: any) => {
-        res.send('Hello World!')
-    })
-
-    app.listen(port, () => { //o método listen é usado para fazer o servidor escutar requisições
-        console.log(`Server is running on port ${port}`)
-    })
-
-    const router = Router();
-
-    const userController = new UserController();
-
-    //router.post('/login', userController.login);
-    app.post('/register', userController.create);
-    // router.get('/users', userController.get);
-    // router.get('/users/:id', userController.getById);
-    // router.put('/users/:id', userController.update);
-    // router.patch('/users/:id', userController.patch);
-
-}).catch(error => console.log(error))
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
